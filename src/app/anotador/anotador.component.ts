1a2daea49b09ea5a7b43593275348e77a67a5134
Sidney Leal <sidleal@gmail.com>
2017-07-25 23:37:05 -0300

    melhorando telas para carregar os textos

--- src/app/anotador/anotador.component.ts
+++ src/app/anotador/anotador.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AuthService } from '../providers/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-anotador',
@@ -26,10 +27,14 @@ export class AnotadorComponent implements OnInit {
   corpusGenre: string;
 
   productionTitle: string;
+  productionSubTitle: string;
+  productionAuthor: string;
   productionPublished: string;
+  productionSource: string;
   productionContent: string;
+  productionRawContent: string;
 
-  constructor(private authService: AuthService, public af: AngularFireDatabase) {
+  constructor(private authService: AuthService, public af: AngularFireDatabase, private router: Router) {
     this.listCorpora();
    }
 
@@ -68,6 +73,10 @@ export class AnotadorComponent implements OnInit {
     this.corpora.push({name: this.corpusName, source: this.corpusSource, genre: this.corpusGenre});
     this.listCorpora();
   }
+  
+  backToMenu() {
+    this.router.navigate(['']);
+  }
 
   listProductions() {
     this.stage = "productions";
@@ -99,14 +108,95 @@ export class AnotadorComponent implements OnInit {
 
   saveProduction() {
     this.productions.push(
-      {title: this.productionTitle, published: this.productionPublished}
+      {
+        title: this.productionTitle, 
+        published: this.productionPublished, 
+        author: this.productionAuthor,
+        source: this.productionSource,
+        rawContent: this.productionRawContent
+      }
     ).then((item) => {
       this.texts = this.af.list('/corpora/' + this.selectedCorpusId + "/productions/" + item.key + "/texts");
       this.texts.push(
-        {title: this.productionTitle, content: this.productionContent, level: 1}
+        {
+          title: this.productionTitle,
+          subTitle: this.productionSubTitle, 
+          content: this.productionContent, 
+          level: 1
+        }
       );
     });
     this.listProductions();
+  }
+
+
+  changeListener(event) {
+
+    var reader:FileReader = new FileReader();
+    
+    this.productionTitle = '';
+    this.productionSubTitle = '';
+    this.productionAuthor = '';
+    this.productionPublished = '';
+    this.productionSource = '';
+    
+    reader.onloadend = (e) => {
+        //console.log(reader.result);
+        this.productionRawContent = reader.result;
+
+        var linhas: string[] = reader.result.split("\n");
+        var parsedText: string = '';
+
+        linhas.forEach(element => {
+          //console.log(element);
+          var meta: boolean = false;
+          
+          var match;
+          
+          match = /<title>(.*)<\/title>/g.exec(element);
+          if (match) {
+            this.productionTitle = match[1];
+            meta = true;
+          }
+
+          match = /<subtitle>(.*)<\/subtitle>/g.exec(element);
+          if (match) {
+            if (!this.productionSubTitle) {
+              this.productionSubTitle = match[1];
+            } else {
+              parsedText += "\n" + match[1] + "\n\n";
+            }
+            meta = true;
+          }
+
+          match = /<author>(.*)<\/author>/g.exec(element);
+          if (match) {
+            this.productionAuthor = match[1];
+            meta = true;
+          }
+
+          match = /<date>(.*)<\/date>/g.exec(element);
+          if (match) {
+            this.productionPublished = match[1];
+            meta = true;
+          }
+
+          match = /<url>(.*)<\/url>/g.exec(element);
+          if (match) {
+            this.productionSource = match[1];
+            meta = true;
+          }
+
+          if (!meta) {
+            parsedText += element + "\n";
+          }
+
+        });
+
+        this.productionContent = parsedText;
+
+    }
+    reader.readAsText(event.target.files[0], 'UTF-8');
   }
 
 }
